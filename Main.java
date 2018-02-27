5392dd6302b9a12207250f84113d36e54a2202b3
Akshay Sharma <sharmaakshay177@gmail.com>
2018-02-27 14:02:34 +0530

    main java file first commit

---
+++ Main.java
@@ -0,0 +1,50 @@
+package snakeLadd;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+public class Main {
+
+	static int getdice()
+	{
+		Random rg = new Random();
+		int randomInt = rg.nextInt(6);
+	    return randomInt;	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Board b1 = new Board();
+		
+	 
+		//System.out.println("dice count "+(getdice()+1));
+		HashMap<Integer, Integer>ladder = new HashMap<Integer,Integer>();
+		ladder.p
+	}
+
+}//main class containing main method
+
+class Board{
+  final	static int endPoint = 100;
+		
+	
+    HashMap<Integer, Integer>ladder = new HashMap<Integer,Integer>();
+	HashMap<Integer, Integer>snake = new HashMap<Integer,Integer>();
+	ladder.put(key, value)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+}//ending of board class
